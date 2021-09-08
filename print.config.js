f047c1deac34c6779ece70a7a645083ccc3cde3e
cq2217669 <85834314+cq2217669@users.noreply.github.com>
2021-09-08 17:05:30 +0800

    Add files via upload
    u

---
+++ print.config.js
@@ -0,0 +1,13 @@
+
+//document.write("<link href='http://192.168.31.149:90/hiprint/hiprint.css' rel='stylesheet' />");
+//document.write("<link href='http://192.168.31.149:90/hiprint/print-lock.css' rel='stylesheet' />");
+//document.write("<script language=javascript src='https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js'></script>");
+//document.write("<script language=javascript src='http://192.168.31.149:90/hiprint/custom_test/print-data.js'></script>");
+//document.write("<script language=javascript src='http://192.168.31.149:90/hiprint/polyfill.min.js'></script>");
+//document.write("<script language=javascript src='http://192.168.31.149:90/hiprint/plugins/jquery.minicolors.min.js'></script>");
+//document.write("<script language=javascript src='http://192.168.31.149:90/hiprint/hiprint.bundle.js'></script>");
+//document.write("<script language=javascript src='http://192.168.31.149:90/hiprint/plugins/jquery.hiwprint.js'></script>");
+
+function alertEx(){
+	alert('123123');
+}
